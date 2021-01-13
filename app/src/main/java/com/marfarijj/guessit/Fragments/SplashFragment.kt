48d98bf4ca4eb1e277e0183a86ec07ce2c7ffd95
JahangirJadi <jahangir.jadi@gmail.com>
2021-01-13 15:21:38 +0500

    My First Commit

---
+++ app/src/main/java/com/marfarijj/guessit/Fragments/SplashFragment.kt
@@ -0,0 +1,50 @@
+package com.marfarijj.guessit.Fragments
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.marfarijj.guessit.R
+
+class SplashFragment : Fragment() {
+
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val view = inflater.inflate(R.layout.fragment_splash, container, false)
+
+        initView(view)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+    }
+
+    private fun initView(view: View?) {
+
+
+        val r2 = Runnable {
+            navController.navigate(R.id.action_nav_to_home)
+
+        }
+        Handler().postDelayed(r2, 5000)
+
+
+
+    }
+}
---
+++ app/src/main/java/com/marfarijj/guessit/Fragments/StartScreenFragment.kt
@@ -0,0 +1,22 @@
+package com.marfarijj.guessit.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.marfarijj.guessit.R
+
+
+class StartScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start_screen, container, false)
+    }
+
+
+}
